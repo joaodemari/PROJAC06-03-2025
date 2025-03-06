d4a725e15c1d497c5970170ab7e2a3f23b3fc210
João Pedro Demari <119618931+joaodemari@users.noreply.github.com>
2025-03-06 23:13:54 +0000

    rename folder

---
+++ projeto1a/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+        public static void main(String[] args) throws Exception {
+            System.out.println("\nHello!");
+            String version = System.getProperty("java.version");
+            System.out.println("Running Java Version "+version+"\n");
+        }
+    
+}
